e5a64b152a556745be9536918f01ce977e47314f
SpreadZhao <www.spreadzhao@outlook.com>
2023-04-13 17:10:54 +0800

    minimize

--- src/questions/MinimizeMaximumOfArray.kt
+++ src/questions/MinimizeMaximumOfArray.kt
@@ -1,16 +1,37 @@
 package questions
 
+import java.math.BigInteger
 import kotlin.math.ceil
 import kotlin.math.max
 
+/**
+ * Link: [Minimize Maximum of Array](https://leetcode.com/problems/minimize-maximum-of-array/)
+ */
+
 class MinimizeMaximumOfArray {
     fun minimizeArrayValue(nums: IntArray): Int {
-        var res = 0L
+        var res = 0
+        var sum = 0
+        for(i in nums.indices){
+            sum += nums[i]
+            val ceil = if(sum % (i + 1) == 0) sum / (i + 1)
+                        else sum / (i + 1) + 1
+            res = max(res, ceil)
+        }
+        return res
+    }
+
+    fun minimizeArrayValue2(nums: IntArray): Int {
+        var res = 0
         var sum = 0L
         for(i in nums.indices){
             sum += nums[i]
-            res = max(res, ceil(sum / (i + 1).toDouble()).toLong())
+            val ceil = if(sum % (i + 1) == 0L) sum / (i + 1)
+                        else sum / (i + 1) + 1
+            res = max(res, ceil.toInt())
         }
-        return res.toInt()
+        return res
     }
+
+
 }
